224a8d320675615199df4bf5c02169fe9336991c
Matthew Whitener <matt.whitener@outlook.com>
2023-01-07 23:14:02 -0500

    changed variables

--- main.tf
+++ main.tf
@@ -22,3 +22,11 @@ resource "azurerm_resource_group" "rg" {
     User        = "mwhitener"
   }
 }
+
+#Creating the Azure Virtual Network
+resource "azurerm_virtual_network" "rg-net" {
+  name                = var.virtual_network_name
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  address_space       = ["172.16.0.0/16"]
+}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
+# Resource Group Variables
 variable "resource_group_name" {
   default = "SplunkTestLab"
+}
+variable "resource_group_location" {
+  default = "eastus2"
+}
+# Network Variables
+variable "virtual_network_name" {
+  default = "splk-test-vnet"
+}
+# Subnet Variables
+variable "virtual_subnet_name" {
+  default = "splk-test-subnet"
 }
